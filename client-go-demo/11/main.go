4e7a4df483da9398ccb738cc6c720d02183f07d0
liky <njleitong@163.com>
2023-01-09 00:27:29 +0800

    client go

---
+++ client-go-demo/11/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/liky/client-go-demo/11/pkg"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
+)
+
+func main() {
+	// 1. config
+	// 2. client
+	// 3. informer
+	// 4. add event handler
+	// 5. informer.Start
+
+	// 集群外部获取 config
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		// 集群内部获取 config
+		inClusterConfig, err := rest.InClusterConfig()
+		if err != nil {
+			log.Fatalln("can't get config")
+		}
+		config = inClusterConfig
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalln("can't crate client")
+	}
+
+	factory := informers.NewSharedInformerFactory(clientset, 0)
+
+	serviceInformer := factory.Core().V1().Services()
+	ingressInformer := factory.Networking().V1().Ingresses()
+
+	controller := pkg.NewController(clientset, serviceInformer, ingressInformer)
+	stopCh := make(chan struct{})
+	factory.Start(stopCh)
+	// 所有全部同步完成
+	factory.WaitForCacheSync(stopCh)
+
+	controller.Run(stopCh)
+}
