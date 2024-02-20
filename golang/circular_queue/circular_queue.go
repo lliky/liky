959b9cbe9384753b82274824494999d3af80dc8e
liky <njleitong@gmail.com>
2024-02-20 18:11:05 +0800

    circular queue

--- golang/circular_queue/circular_queue.go
+++ golang/circular_queue/circular_queue.go
@@ -1,1 +1,39 @@
 package circular_queue
+
+type CircularQueue struct {
+	data  []interface{}
+	size  int
+	front int
+	rear  int
+}
+
+func NewCircularQueue(size int) *CircularQueue {
+	return &CircularQueue{
+		data:  make([]interface{}, size),
+		size:  size,
+		front: 0,
+		rear:  0,
+	}
+}
+
+func (c *CircularQueue) Enqueue(value interface{}) bool {
+	if c.rear == c.front+c.size {
+		return false
+	}
+	c.data[c.rear%c.size] = value
+	c.rear++
+	return true
+}
+
+func (c *CircularQueue) Dequeue() (interface{}, bool) {
+	if c.rear == c.front {
+		return nil, false
+	}
+	value := c.data[c.front%c.size]
+	c.front++
+	return value, true
+}
+
+func (c *CircularQueue) Len() int {
+	return c.rear - c.front
+}
--- golang/circular_queue/circular_queue_test.go
+++ golang/circular_queue/circular_queue_test.go
@@ -1,1 +1,53 @@
 package circular_queue
+
+import "testing"
+
+func TestNewCircularQueue(t *testing.T) {
+	results := []struct {
+		op       string
+		data     []int
+		size     int
+		deCount  int
+		expected int
+	}{
+		{
+			op:       "fullQueue",
+			data:     []int{1, 2, 3, 4, 5, 6},
+			size:     5,
+			deCount:  0,
+			expected: 5,
+		},
+		{
+			op:       "emptyQueue",
+			data:     []int{1, 2, 3, 4},
+			size:     5,
+			deCount:  4,
+			expected: 0,
+		},
+		{
+			op:       "enqueue",
+			data:     []int{1, 2, 3, 4},
+			size:     5,
+			deCount:  0,
+			expected: 4,
+		},
+	}
+
+	for _, result := range results {
+		t.Run(result.op, func(t *testing.T) {
+			c := NewCircularQueue(result.size)
+			for _, value := range result.data {
+				c.Enqueue(value)
+			}
+			for i := 0; i < result.deCount; i++ {
+				value, _ := c.Dequeue()
+				if value != result.data[i] {
+					t.Errorf("Expected %d, got %d", result.data[i], value)
+				}
+			}
+			if c.Len() != result.expected {
+				t.Errorf("The length of Queue expected %d, got %d", result.expected, c.Len())
+			}
+		})
+	}
+}
