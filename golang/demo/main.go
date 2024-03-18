7bf4920d7540dd8ff1fbf3be9120478b700f9716
liky <njleitong@gmail.com>
2024-03-18 19:57:49 +0800

    memory model

--- golang/demo/main.go
+++ golang/demo/main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var a = make(map[string]int)
-	exp := a["bc"]
-	a["bc"] = a["bc"] + 1
-	fmt.Println(exp, a)
+	fmt.Println(make(chan int))
+	for i := 1; i < 16; i++ {
+		and := i & 3
+		fmt.Println(i, " & 3 = ", and)
+	}
+	fmt.Println(alignUp(12, 4))
+}
+
+func alignUp(n, a uintptr) uintptr {
+	return (n + a - 1) &^ (a - 1)
 }
