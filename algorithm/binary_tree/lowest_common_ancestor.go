14a95243364d5577ac31fd06cb6658293bfe3d03
liky <njleitong@gamil.com>
2024-06-13 17:04:14 +0800

    binary tree

---
+++ algorithm/binary_tree/lowest_common_ancestor.go
@@ -0,0 +1,41 @@
+package binary_tree
+
+// 最近公共祖先
+// leetcode 236
+
+func lowestCommonAncestor(root, p, q *Node) *Node {
+	// 每个节点记录父节点
+	parentMap := make(map[*Node]*Node)
+	parentMap[root] = nil
+	processLCA(root, parentMap)
+
+	cur := p
+	pMap := make(map[*Node]struct{})
+	pMap[p] = struct{}{}
+	for cur != nil {
+		cur = parentMap[cur]
+		pMap[cur] = struct{}{}
+	}
+	cur = q
+	for cur != nil {
+		if _, ok := pMap[cur]; ok {
+			return cur
+		}
+		cur = parentMap[cur]
+	}
+	return nil
+}
+
+func processLCA(root *Node, parentMap map[*Node]*Node) {
+	if root == nil {
+		return
+	}
+	if root.Left != nil {
+		parentMap[root.Left] = root
+		processLCA(root.Left, parentMap)
+	}
+	if root.Right != nil {
+		parentMap[root.Right] = root
+		processLCA(root.Right, parentMap)
+	}
+}
