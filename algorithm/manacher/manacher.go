c7b7f4f16aefb82d354e72af564ba4f2ed7f1854
liky <njleitong@gmail.com>
2024-07-16 01:41:45 +0800

    manacher

--- algorithm/manacher/manacher.go
+++ algorithm/manacher/manacher.go
@@ -20,3 +20,25 @@ func manacherString(s string) []byte {
 	}
 	return res
 }
+
+func manacher(s string)  {
+	str := manacherString(s)
+	pArr := make([]int, len(str))
+	R, C := 0, -1
+	for i := 0; i < len(str);i++ {
+		if i 在 R 外部 {
+			 从 i 开始往两边暴力扩
+		} else {
+			if i‘ 回文区域彻底在 L...R 内 {
+				pArr[i] = O(1) 表达式
+			} else if i'' 回文区域有一部分在 L... R 外{
+				pArr[i] = O(1) 表达式
+			} else { i'' 回文区域和 L...R 的左边界压线
+				从 R 之外的字符开始，往外扩，然后确定 pArr[i] 的答案
+
+			}
+		}
+
+		更新 R,C 
+	}
+}
