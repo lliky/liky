372758110364b7abe8210d73bb6e32d06b6ec985
liky <njleitong@gamil.com>
2024-07-16 11:35:55 +0800

    manacher

--- algorithm/manacher/manacher.go
+++ algorithm/manacher/manacher.go
@@ -4,7 +4,8 @@ func MaxLcpsLength(s string) int {
 	if len(s) == 0 {
 		return 0
 	}
-	return 0
+	res, _ := manacher1(s)
+	return res - 1
 }
 
 func manacherString(s string) []byte {
@@ -21,24 +22,60 @@ func manacherString(s string) []byte {
 	return res
 }
 
-func manacher(s string)  {
+func manacher1(s string) (r, index int) {
 	str := manacherString(s)
-	pArr := make([]int, len(str))
-	R, C := 0, -1
-	for i := 0; i < len(str);i++ {
-		if i 在 R 外部 {
-			 从 i 开始往两边暴力扩
+	d := make([]int, len(str))
+	R, C := -1, 0 // R 代表右边界, C 代表中心点
+	for i := 0; i < len(str); i++ {
+		if R < i {
+			//暴力扩
+			for i-d[i] >= 0 && i+d[i] < len(str) && str[i-d[i]] == str[i+d[i]] {
+				d[i]++
+			}
 		} else {
-			if i‘ 回文区域彻底在 L...R 内 {
-				pArr[i] = O(1) 表达式
-			} else if i'' 回文区域有一部分在 L... R 外{
-				pArr[i] = O(1) 表达式
-			} else { i'' 回文区域和 L...R 的左边界压线
-				从 R 之外的字符开始，往外扩，然后确定 pArr[i] 的答案
-
+			// L = 2 *C - R
+			// i' = 2 *C - i   l' = i' - d[i'] + 1
+			if 2*C-i-d[2*C-i]+1 > 2*C-R { // i' 的范围完全在 [L...R] 里面
+				d[i] = d[2*C-i]
+			} else if 2*C-i-d[2*C-i]+1 < 2*C-R { //  i' 的范围部分不在 [L...R] 里面
+				d[i] = R - i + 1
+			} else { // i' 左边界 l' 恰好和 L 重合
+				d[i] = R - i
+				for i-d[i] >= 0 && i+d[i] < len(str) && str[i-d[i]] == str[i+d[i]] {
+					d[i]++
+				}
 			}
 		}
-
-		更新 R,C 
+		if r < d[i] {
+			r = d[i]
+			index = i
+		}
+		if C+d[i]-1 >= R {
+			C = i
+			R = C + d[i] - 1
+		}
 	}
+	return
 }
+
+//func manacher(s string)  {
+//	str := manacherString(s)
+//	pArr := make([]int, len(str))
+//	R, C := 0, -1
+//	for i := 0; i < len(str);i++ {
+//		if i 在 R 外部 {
+//			 从 i 开始往两边暴力扩
+//		} else {
+//			if i‘ 回文区域彻底在 L...R 内 {
+//				pArr[i] = O(1) 表达式
+//			} else if i'' 回文区域有一部分在 L... R 外{
+//				pArr[i] = O(1) 表达式
+//			} else { i'' 回文区域和 L...R 的左边界压线
+//				从 R 之外的字符开始，往外扩，然后确定 pArr[i] 的答案
+//
+//			}
+//		}
+//
+//		更新 R,C
+//	}
+//}
--- algorithm/manacher/manacher_test.go
+++ algorithm/manacher/manacher_test.go
@@ -2,6 +2,7 @@ package manacher
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -10,4 +11,37 @@ func TestManacher(t *testing.T) {
 	fmt.Println(string(manacherString(s1)))
 	s2 := "121"
 	fmt.Println(string(manacherString(s2)))
+}
+
+func TestManacher1(t *testing.T) {
+	s := "abcb"
+	fmt.Println(manacher1(s))
+}
+
+func TestMaxLcpsLength(t *testing.T) {
+	testCases := []struct {
+		s        string
+		expected int
+	}{
+		{
+			s:        "aaaaa",
+			expected: 5,
+		},
+		{
+			s:        "abcdefg",
+			expected: 1,
+		},
+		{
+			s:        "123216",
+			expected: 5,
+		},
+		{
+			s:        "1221",
+			expected: 4,
+		},
+	}
+	for _, testCase := range testCases {
+		actual := MaxLcpsLength(testCase.s)
+		require.Equal(t, testCase.expected, actual)
+	}
 }
