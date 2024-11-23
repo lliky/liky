3e5e3ff5f2548184096ba6a0e94717ef8d97de16
liky <njleitong@gmail.com>
2024-11-24 02:31:33 +0800

    client-go

---
+++ kubernetes/client-go/rest-client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	// config
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		panic(err)
+	}
+
+	config.GroupVersion = &v1.SchemeGroupVersion
+	config.APIPath = "/api"
+	config.NegotiatedSerializer = scheme.Codecs
+	// client
+	client, err := rest.RESTClientFor(config)
+	if err != nil {
+		panic(err)
+	}
+	// get data
+	pod := v1.Pod{}
+	err = client.Get().Namespace("default").Resource("pods").Name("test").Do(context.TODO()).Into(&pod)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("pod.Name: %v\n", pod.Name)
+	}
+}
